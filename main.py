d5b5bd497365133ba83262ba23fced9b99bd44cb
solane402 <ishaan301@gmail.com>
2025-01-10 20:06:02 -0500

    added a function that returns number of characters

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
+letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
 def main() :
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        
+        words = file_contents.split()
+        print(len(words))
+        dict = number_of_chars(file_contents)
+        print(dict)
+
+def number_of_chars(input) :
+    num_of_letters = {}
+    for char in input.lower():
+        if char in letters:
+            try :
+                num_of_letters[char] += 1
+            except Exception :
+                num_of_letters[char] = 1
+            
+    return num_of_letters 
 
 
 
